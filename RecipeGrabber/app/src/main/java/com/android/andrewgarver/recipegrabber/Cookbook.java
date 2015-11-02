d2320a97de54ba66b6defe28636c9f1bf8295386
Andrew-Garver <mormonteach@hotmail.com>
2015-11-02 16:34:13 -0700

    added stuff day 1

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
@@ -6,6 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 /**
  * Created by Andrew Garver on 11/2/2015.
@@ -15,6 +19,18 @@ public class Cookbook extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_cookbook, container, false);
+
+        String[] items = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
+                        R.layout.row_layout,
+                        items);
+
+        View view = inflater.inflate(R.layout.frag_cookbook, container, false);
+        ListView list = (ListView) view.findViewById(R.id.listView);
+        list.setAdapter(adapter);
+
+
+        return view;
     }
 }
