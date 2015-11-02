59f824cc339786ff2508605f412e735a13f58660
Andrew-Garver <mormonteach@hotmail.com>
2015-11-02 12:25:44 -0700

    got it

---
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/ShoppingList.java
@@ -0,0 +1,20 @@
+package com.android.andrewgarver.recipegrabber;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Andrew Garver on 11/2/2015.
+ */
+public class ShoppingList extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_shoppinglist, container, false);
+    }
+}
