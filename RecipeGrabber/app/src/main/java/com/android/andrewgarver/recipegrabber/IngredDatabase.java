53dd9ce9f1a17f6cc52c6a013bae7570544b4732
Landon Jamieson <landonjamieson@gmail.com>
2015-11-05 22:39:27 -0700

    Created those unit tests as far as I can understand them.

---
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/IngredDatabase.java
@@ -0,0 +1,14 @@
+package com.android.andrewgarver.recipegrabber;
+
+/**
+ * Created by Landon on 11/5/2015.
+ */
+public class IngredDatabase {
+    public String fetchIngred(int id) {
+        return "";
+    }
+
+    public int addIngred(String newIngred) {
+        return 0;
+    }
+}
--- RecipeGrabber/app/src/main/java/tests/ExampleTest.java
+++ RecipeGrabber/app/src/main/java/tests/ExampleTest.java
@@ -2,14 +2,32 @@ package tests;
 
 import android.test.InstrumentationTestCase;
 
+import com.android.andrewgarver.recipegrabber.IngredDatabase;
+
 /**
  * Created by Andrew Garver on 11/4/2015.
  */
 public class ExampleTest extends InstrumentationTestCase {
-    public boolean callDataBase() throws Exception {
+    public boolean testCallDataBase() throws Exception {
         boolean accessedDB = false;
         assert(accessedDB);
         return true;
+    }
+
+    IngredDatabase testDatabase = new IngredDatabase();
+
+    public void testAddIndred() throws Exception {
+        assert 0 == testDatabase.addIngred("Baby Carrots");
+        assert 1 == testDatabase.addIngred("Salt");
+        assert 2 == testDatabase.addIngred("Baking Soda");
+    }
+
+    public void testFetchIndred() throws Exception {
+        assert testDatabase.fetchIngred(0).equals("");
+        testAddIndred();
+        assert testDatabase.fetchIngred(0).equals("Baby Carrots");
+        assert testDatabase.fetchIngred(1).equals("Salt");
+        assert testDatabase.fetchIngred(2).equals("Baking Soda");
     }
 }
 
