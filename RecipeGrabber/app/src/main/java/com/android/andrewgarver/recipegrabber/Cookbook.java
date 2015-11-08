5deb5c306cf83c0f51fa25524c44ba95724a41f2
Andrew-Garver <mormonteach@hotmail.com>
2015-11-07 17:13:37 -0700

    Added pictures to listView

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
@@ -1,38 +1,57 @@
 package com.android.andrewgarver.recipegrabber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by Andrew Garver on 11/2/2015.
  */
 public class Cookbook extends Fragment {
 
+    ListView list;
+    String[] items = {"Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly",
+            "Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly",
+            "Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly"};
+
+    Integer[] imageId = { R.drawable.burger, R.drawable.burger, R.drawable.burger, R.drawable.burger,
+            R.drawable.burger, R.drawable.burger, R.drawable.burger, R.drawable.burger,
+            R.drawable.burger, R.drawable.burger, R.drawable.burger, R.drawable.burger,
+            R.drawable.burger, R.drawable.burger, R.drawable.burger };
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        String[] items = {"Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly",
-                          "Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly",
-                          "Pancakes", "Pizza", "Burger", "Spaghetti", "Peanut Butter and Jelly"};
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(),
-                        R.layout.row_layout,
-                        items);
-
         View view = inflater.inflate(R.layout.frag_cookbook, container, false);
-        ListView list = (ListView) view.findViewById(R.id.listView);
-        list.setAdapter(adapter);
 
+        CustomList adapter = new CustomList(getActivity(), items, imageId);
+        list = (ListView) view.findViewById(R.id.listView);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity(), "Congrats, You clicked something", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        ImageButton addBtn = (ImageButton) view.findViewById(R.id.addRecipe);
+            addBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), AddRecipe.class));
+            }
+        });
 
         return view;
     }
+
+
 }
--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/ShoppingList.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/ShoppingList.java
@@ -1,5 +1,6 @@
 package com.android.andrewgarver.recipegrabber;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
 
 /**
@@ -27,6 +29,13 @@ public class ShoppingList extends Fragment {
         View view = inflater.inflate(R.layout.frag_shoppinglist, container, false);
         ListView list = (ListView) view.findViewById(R.id.listView);
         list.setAdapter(adapter);
+
+        ImageButton addBtn = (ImageButton) view.findViewById(R.id.addToShoppingList);
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(getContext(), AddToShoppingList.class));
+            }
+        });
 
 
         return view;
