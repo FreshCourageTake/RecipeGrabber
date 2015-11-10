60dfce510e278719ddc90e3bf615550103ae7383
Andrew-Garver <mormonteach@hotmail.com>
2015-11-10 11:51:40 -0700

    Made add recipe activity scrollable

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/AddRecipe.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/AddRecipe.java
@@ -9,6 +9,8 @@ import android.view.View;
 
 public class AddRecipe extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,14 +18,6 @@ public class AddRecipe extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
