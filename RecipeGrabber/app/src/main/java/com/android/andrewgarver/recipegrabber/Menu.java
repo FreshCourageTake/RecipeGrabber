e3a0bd665ab70e73c9680312cf94fb9f40130b35
Landon <landonjamieson@gmail.com>
2015-11-19 12:12:05 -0700

    Added some log statements for information and debugging.

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Menu.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Menu.java
@@ -6,6 +6,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -22,9 +23,7 @@ import java.util.Locale;
  * Created by Andrew Garver on 11/2/2015.
  */
 public class Menu extends Fragment {
-
-
-
+    private static final String TAG = Menu.class.getSimpleName();
     private ListView listView;
     private GregorianCalendar gCalender;
 
@@ -32,7 +31,7 @@ public class Menu extends Fragment {
     private RecyclerView recyclerView;
     private DayAdapter dayAdapter;
 
-    int num_month_days = 31;
+    private int num_month_days = 31;
 
     @Nullable
     @Override
@@ -73,6 +72,13 @@ public class Menu extends Fragment {
 
     public static List<CalDay> getDay(int num_month_days) {
         List<CalDay> numDays = new ArrayList<>();
+
+        //log the number of days
+        Log.i(TAG, "The number of days in the month are " + num_month_days);
+
+        //error log ensures that the number of days is valid
+        if (num_month_days > 31 || num_month_days < 28)
+            Log.e(TAG, num_month_days + " is not a valid number of days for any month. Change num_month_days.");
 
         String[] days = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14",
                 "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28",
