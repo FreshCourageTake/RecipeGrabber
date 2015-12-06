60e802c44a2c8421f592ee072cf86f1ab150a839
reeds-git <freefreefree2013@gmail.com>
2015-12-06 02:04:11 -0700

    added some content to add event to calendar

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Menu.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Menu.java
@@ -1,10 +1,13 @@
 package com.android.andrewgarver.recipegrabber;
 
+import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.format.Time;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +15,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.android.andrewgarver.recipegrabber.extendCalView.CalendarProvider;
 import com.android.andrewgarver.recipegrabber.extendCalView.Event;
@@ -23,6 +25,8 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.Locale;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Andrew Garver on 11/2/2015.
@@ -44,6 +48,37 @@ public class Menu extends Fragment {
                 R.layout.row_layout,
                 items);
 
+        ContentValues values = new ContentValues();
+        values.put(CalendarProvider.COLOR, Event.COLOR_RED);
+
+        values.put(CalendarProvider.DESCRIPTION, "Some Description");
+        values.put(CalendarProvider.LOCATION, "Some location");
+        values.put(CalendarProvider.EVENT, "Event name");
+
+        Calendar cal = Calendar.getInstance();
+        TimeZone tz = TimeZone.getDefault();
+
+
+        int year = 2015;
+        int month = 12;
+        int day = 15;
+        int hour = 12;
+        int min = 12;
+
+        int julianDay = Time.getJulianDay(cal.getTimeInMillis(), TimeUnit.MILLISECONDS.toSeconds(tz.getOffset(cal.getTimeInMillis())));
+        cal.set(year, month, day, hour, min);
+        values.put(CalendarProvider.START, cal.getTimeInMillis());
+        values.put(CalendarProvider.START_DAY, julianDay);
+
+
+        cal.set(year, month, day, hour, min + 5);
+        int endDayJulian = Time.getJulianDay(cal.getTimeInMillis(), TimeUnit.MILLISECONDS.toSeconds(tz.getOffset(cal.getTimeInMillis())));
+
+        values.put(CalendarProvider.END, cal.getTimeInMillis());
+        values.put(CalendarProvider.END_DAY, endDayJulian);
+
+       // Uri uri = getContentResolver().insert(CalendarProvider.CONTENT_URI, values);
+      //  extendedCalendarView.setBackgroundColor(5);
 
         Log.i(TAG, "The day of the month is ");
 
@@ -71,6 +106,5 @@ public class Menu extends Fragment {
     }
 
 
-    ContentValues values = new ContentValues();
-    //values.put(CalendarProvider.COLOR, Event.COLOR_RED);
+
 }
