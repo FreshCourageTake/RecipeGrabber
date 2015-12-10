7b8d0d08c6354cb257512c77ea93e79a6357957c
Landon <landonjamieson@gmail.com>
2015-12-10 02:25:43 -0700

    Added ability to delete recipe from cookbook. Does not yet actually delete from the database, only the listview. Will do tomorrow.

--- RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
+++ RecipeGrabber/app/src/main/java/com/android/andrewgarver/recipegrabber/Cookbook.java
@@ -1,6 +1,8 @@
 package com.android.andrewgarver.recipegrabber;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -14,6 +16,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
@@ -34,7 +37,7 @@ public class Cookbook extends Fragment {
 
         View view = inflater.inflate(R.layout.frag_cookbook, container, false);
         dbHelper = new DatabaseAdapter(getActivity());
-        ArrayList<String> items = dbHelper.getAllRecipes();
+        final ArrayList<String> items = dbHelper.getAllRecipes();
 
         adapter = new ArrayAdapter<>(getContext(), R.layout.row_layout, items);
         list = (ListView) view.findViewById(R.id.listView);
@@ -54,11 +57,38 @@ public class Cookbook extends Fragment {
             }
         });
 
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                final String toDel = adapter.getItem(position);
+                AlertDialog.Builder adb = new AlertDialog.Builder(getContext());
+                adb.setTitle("Delete recipe: " + toDel + '?');
+                adb.setMessage("Are you sure you want to remove this recipe?");
+                adb.setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        adapter.remove(toDel);
+                        //need to remove from database still
+                        Toast.makeText(getContext(), "Recipe Deleted", Toast.LENGTH_LONG).show();
+                    }
+                });
+                adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+
+                AlertDialog ad = adb.create();
+                ad.show();
+                return true;
+            }
+        });
+
         ImageButton addBtn = (ImageButton) view.findViewById(R.id.addRecipe);
-            addBtn.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    startActivityForResult(new Intent(getContext(), AddRecipe.class), newRecipeCode);
-                }
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivityForResult(new Intent(getContext(), AddRecipe.class), newRecipeCode);
+            }
             });
 
         return view;
